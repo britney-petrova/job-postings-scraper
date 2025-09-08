ad2fbda799cda9ed27cda8ea4d8739462b17e87c
Britney <petrovabritney@gmail.com>
2025-09-09 00:43:39 +0300

    Add CATEGORY_URL and EMAIL_RECIPIENT constants

--- scraper/config.py
+++ scraper/config.py
@@ -1,1 +1,4 @@
-# keyword list, constants
+# the URL of the WWR category to be scraped
+CATEGORY_URL = "https://weworkremotely.com/categories/remote-full-stack-programming-jobs"
+# the email address where scraped job postings (or notifs) will be sent
+EMAIL_RECIPIENT = "petrovabritney@gmail.com"
